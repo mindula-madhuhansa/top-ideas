2018361d9b5a1134ac37dc9218d0bb1760c6235c
mindula-madhuhansa <mimmindula@gmail.com>
2024-04-30 21:15:29 +0530

    add new idea

--- src/app/AddNewIdea.jsx
+++ src/app/AddNewIdea.jsx
@@ -1,3 +1,52 @@
+import Header from "../components/Header";
+
+import { ArrowLeft, SendIcon } from "lucide-react";
+
 export default function AddNewIdea() {
-  return <div>Add New Idea</div>;
+  return (
+    <div>
+      <Header />
+
+      <div>
+        <button
+          onClick={() => window.history.back()}
+          className="btn btn-primary mt-4 space-x-2"
+        >
+          <ArrowLeft className="h-5 w-5" />
+          Back
+        </button>
+
+        <h2 className="mt-8 font-bold text-4xl md:text-5xl text-center">
+          From Concept to Creation
+        </h2>
+
+        <form className="mt-16 border p-4 py-8 rounded-lg shadow-md w-full max-w-3xl mx-auto">
+          <div className="flex flex-col space-y-2 w-full max-w-2xl mx-auto">
+            <label className="font-bold text-sm opacity-80">Your Name*</label>
+            <input
+              type="text"
+              placeholder="John Doe"
+              className="input input-bordered"
+            />
+          </div>
+
+          <div className="flex flex-col mt-8 space-y-2  w-full max-w-2xl mx-auto">
+            <label className="font-bold text-sm opacity-80">Your Idea*</label>
+            <textarea
+              className="textarea textarea-bordered border-primary"
+              placeholder="Describe your idea here..."
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="btn flex w-full max-w-2xl mx-auto btn-primary mt-8 uppercase font-bold text-lg"
+          >
+            <SendIcon className="w-6 h-6" />
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,18 +2,25 @@ import { useContext } from "react";
 
 import { ThemeContext } from "../context/ThemeContext";
 import { THEMES } from "../constants";
+import { PlusCircle } from "lucide-react";
 
 export default function Header() {
   const { setTheme } = useContext(ThemeContext);
   return (
     <header className="border flex flex-1 items-center justify-between shadow-lg p-4 md:p-8 rounded-lg">
       <h2 className="font-bold text-2xl md:text-4xl">Top Ideas</h2>
-      <div className="flex items-center space-x-2">
-        <button className="btn btn-primary btn-xs md:btn-lg">New Idea</button>
+      <div className="flex flex-col md:flex-row items-center space-y-2 space-x-0 md:space-x-2 md:space-y-0">
+        <button
+          onClick={() => window.location.assign("/new")}
+          className="btn btn-primary btn-sm md:btn-lg"
+        >
+          <PlusCircle className="h-5 w-5" />
+          New Idea
+        </button>
 
         <select
           onChange={(e) => setTheme(e.target.value)}
-          className="select select-xs md:select-lg  border-primary w-full max-w-xs"
+          className="select select-sm md:select-lg  border-primary w-full max-w-xs"
         >
           <option disabled selected>
             Theme
